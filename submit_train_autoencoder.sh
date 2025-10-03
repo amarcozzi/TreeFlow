674eb7e5ab9d9719a0641c102ca450da556731a5
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-10-03 14:47:10 -0600

    Add SLURM script for training autoencoder generator on GPU

---
+++ submit_train_autoencoder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=umontana_fire_modeling
+#SBATCH --partition=gpu-a100
+#SBATCH --gres=gpu:a100:1
+#SBATCH --job-name="train_autoencoder_generator"
+#SBATCH --cpus-per-task=12
+#SBATCH --time=2-0
+#SBATCH --output=log_train_autoencoder_generator.out
+
+module load cuda
+
+source /project/umontana_fire_modeling/anthony.marcozzi/miniforge3/etc/profile.d/conda.sh
+conda activate canopy-flow
+
+srun python train_autoencoder.py
