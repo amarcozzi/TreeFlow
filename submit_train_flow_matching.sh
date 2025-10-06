14fb27b526691d49fad2a4bb57900c3afe5431fa
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-10-06 09:00:35 -0600

    Add SLURM submission script for Flow Matching training: configure job parameters and environment setup

---
+++ submit_train_flow_matching.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=umontana_fire_modeling
+#SBATCH --partition=gpu-a100
+#SBATCH --gres=gpu:a100:1
+#SBATCH --job-name="train_flow_matching"
+#SBATCH --cpus-per-task=12
+#SBATCH --time=2-0
+#SBATCH --output=log_train_flow_matching.out
+
+module load cuda
+
+source /project/umontana_fire_modeling/anthony.marcozzi/miniforge3/etc/profile.d/conda.sh
+conda activate canopy-flow
+
+srun python train.py
