91f8f0a1f04072585fd1056de29a5f0d986735b3
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-10-06 22:43:10 -0600

    Fix submit_debug.sh: update output log filename for clarity

--- submit_debug.sh
+++ submit_debug.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=umontana_fire_modeling
 #SBATCH --partition=gpu-a100
 #SBATCH --gres=gpu:a100:1
-#SBATCH --job-name="train_flow_matching"
+#SBATCH --job-name="fm_debug"
 #SBATCH --cpus-per-task=36
 #SBATCH --time=2-0
 #SBATCH --output=log_debug.out
