6fbdf1b65ef1409545cb554ede4d39ae0ea70437
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-10-27 04:27:08 -0600

    Add SLURM submission script for transformer model training

---
+++ submit_train_transformer_12_512.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --account=umontana_fire_modeling
+#SBATCH --partition=gpu-a100
+#SBATCH --gres=gpu:a100:1
+#SBATCH --job-name="tf_12_512"
+#SBATCH --cpus-per-task=36
+#SBATCH --time=2-0
+#SBATCH --output=log_train_transformer_12_512.out
+
+module load cuda
+
+source /project/umontana_fire_modeling/anthony.marcozzi/miniforge3/etc/profile.d/conda.sh
+conda activate canopy-flow
+
+python train.py \
+    --output_dir experiments \
+    --experiment_name "transformer-24-256" \
+    --data_path FOR-species20K \
+    --preprocessed_version voxel_0.1m \
+    --model_dim 512 \
+    --num_heads 16 \
+    --num_layers 12 \
+    --dropout 0.1 \
+    --batch_size 8 \
+    --visualize_every 5 \
+    --batch_mode sample_to_min \
+    --rotation_augment \
+    --num_workers 24 \
+    --num_epochs 2000 \
+    --lr 1e-4 \
+    --ode_method dopri5 \
+    --use_amp \
+    --use_flash_attention \
+    --min_visualization_points 2500 \
+    --max_visualization_points 50000
