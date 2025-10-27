825f4e079095feecc3ce44f82bd7d5e98379e300
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2025-10-27 07:16:06 -0600

    Rename SLURM submission script for transformer model and update job parameters

--- submit_train_transformer_32_256.sh
+++ submit_train_transformer_32_256.sh
@@ -2,11 +2,11 @@
 #SBATCH --account=umontana_fire_modeling
 #SBATCH --partition=gpu-a100
 #SBATCH --gres=gpu:a100:1
-#SBATCH --job-name="tf_48_256"
+#SBATCH --job-name="tf_32_256"
 #SBATCH --cpus-per-task=36
 #SBATCH --mem=128G
 #SBATCH --time=2-0
-#SBATCH --output=log_train_transformer_48_256.out
+#SBATCH --output=log_train_transformer_32_256.out
 
 module load cuda
 
@@ -15,12 +15,12 @@ conda activate canopy-flow
 
 python train.py \
     --output_dir experiments \
-    --experiment_name "transformer-48-256" \
+    --experiment_name "transformer-32-256" \
     --data_path FOR-species20K \
     --preprocessed_version voxel_0.1m \
     --model_dim 256 \
     --num_heads 8 \
-    --num_layers 48 \
+    --num_layers 32 \
     --dropout 0.1 \
     --batch_size 8 \
     --visualize_every 5 \
