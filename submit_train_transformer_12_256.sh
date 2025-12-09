cbbed2f5fe05bb6208a6b47efa6fff67ed2673ae
amarcozzi <anthonymarcozzi@gmail.com>
2025-12-09 16:26:24 -0700

    update training script: increase transformer layers to 12, adjust epoch count to 2500, switch to raw preprocessed data

--- submit_train_transformer_12_256.sh
+++ submit_train_transformer_12_256.sh
@@ -2,11 +2,11 @@
 #SBATCH --account=umontana_fire_modeling
 #SBATCH --partition=gpu-a100
 #SBATCH --gres=gpu:a100:1
-#SBATCH --job-name="tf_8_256"
+#SBATCH --job-name="tf_12_256"
 #SBATCH --cpus-per-task=36
 #SBATCH --mem=128G
 #SBATCH --time=2-0
-#SBATCH --output=log_train_transformer_8_256.out
+#SBATCH --output=log_train_transformer_12_256_raw.out
 
 module load cuda
 
@@ -15,21 +15,22 @@ conda activate canopy-flow
 
 python train.py \
     --output_dir experiments \
-    --experiment_name "transformer-8-256-0.2" \
+    --experiment_name "transformer-12-256-raw" \
     --data_path FOR-species20K \
     --csv_path FOR-species20K/tree_metadata_dev.csv \
-    --preprocessed_version voxel_0.2m \
+    --preprocessed_version raw \
     --model_dim 256 \
     --num_heads 8 \
-    --num_layers 8 \
+    --num_layers 12 \
     --dropout 0.1 \
     --batch_size 8 \
     --visualize_every 10 \
+    --save_every 100 \
     --sample_exponent 0.3 \
     --rotation_augment \
     --shuffle_augment \
     --num_workers 24 \
-    --num_epochs 1000 \
+    --num_epochs 2500 \
     --lr 1e-4 \
     --use_amp \
     --cfg_dropout_prob 0.1 \
