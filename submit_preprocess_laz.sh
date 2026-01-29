893e7c7d9333babbc33364eec85e11026a08bd8e
amarcozzi <anthonymarcozzi@gmail.com>
2026-01-29 10:18:47 -0700

    update dataset and evaluation scripts to use flexible preprocessing version for Zarr format

--- submit_preprocess_laz.sh
+++ submit_preprocess_laz.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="preprocess_trees"
 #SBATCH --cpus-per-task=40
 #SBATCH --time=4:00:00
-#SBATCH --output=log_preprocess.out
+#SBATCH --output=log_preprocess_zarr.out
 
 source /project/umontana_fire_modeling/anthony.marcozzi/miniforge3/etc/profile.d/conda.sh
 conda activate canopy-flow
