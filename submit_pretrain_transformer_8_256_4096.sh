11c1a6ce57fe1244a7ef2d1090c60fcef98b0ca3
amarcozzi <anthonymarcozzi@gmail.com>
2026-02-03 21:39:18 -0700

    feat: add SLURM script for pretraining transformer model with specified resources

---
+++ submit_pretrain_transformer_8_256_4096.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --account=umontana_fire_modeling
+#SBATCH --partition=gpu-l40s
+#SBATCH --gres=gpu:l40s:4
+#SBATCH --job-name="pretrain_tf_4096"
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=256G
+#SBATCH --time=1-0
+#SBATCH --output=log_pretrain_transformer_8_256_4096.out
+
+module load cuda
+
+source /project/umontana_fire_modeling/anthony.marcozzi/miniforge3/etc/profile.d/conda.sh
+conda activate treeflow
+
+accelerate launch --num_processes=4 train.py \
+    --output_dir experiments \
+    --experiment_name "pretrain-8-256-4096" \
+    --data_path ./data/preprocessed-16384 \
+    --model_dim 256 \
+    --num_heads 8 \
+    --num_layers 8 \
+    --dropout 0.1 \
+    --batch_size 64 \
+    --visualize_every 10 \
+    --save_every 100 \
+    --rotation_augment \
+    --shuffle_augment \
+    --num_workers 4 \
+    --num_epochs 2000 \
+    --lr 1e-4 \
+    --min_lr 1e-5 \
+    --lr_scheduler cosine \
+    --grad_clip_norm 1.0 \
+    --mixed_precision bf16 \
+    --cfg_dropout_prob 0.15 \
+    --max_points 4096 \
+    --sample_exponent 0.3
